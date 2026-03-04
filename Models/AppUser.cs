83b552d0614e20e9f7a270ab9df24d560f3e65dc
gtodorov_22180064@unwe.bg <gtodorov_22180064@unwe.bg>
2026-03-04 13:23:56 +0200

    komitvam

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,6 +1,11 @@
 ﻿namespace BizSecureDemo22180064.Models;
 public class AppUser
+
 {
+
+    // Brute-force protection
+    public int? FailedLogins { get; set; }
+    public DateTime? LockoutUntilUtc { get; set; }
     public int Id { get; set; }
     public string Email { get; set; } = "";
     public string PasswordHash { get; set; } = "";
